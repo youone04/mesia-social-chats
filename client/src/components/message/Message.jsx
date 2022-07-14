d5a8ccda5f4e265316d8f6746c064b8602cb97a3
youone04 <yudi.gunaone87@gmail.com>
2022-07-14 16:48:31 +0700

    done chat api dan client

--- client/src/components/message/Message.jsx
+++ client/src/components/message/Message.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
+import { format } from "timeago.js";
 import "./message.css";
 
-export default function Message({own}) {
+export default function Message(props) {
   return (
-    <div className={own?'message own':'message'}>
+    <div className={props.own?'message own':'message'}>
         <div className='messageTop'>
             <img src='https://apkvision.com/wp-content/uploads/2020/09/unnamed-1-47.png' alt='' className='messageImage' />
-            <p className='messageText'>Lorem Ipsum is simply dummy</p>
+            <p className='messageText'>{props.message?.text}</p>
         </div>
         <div className='messageBottom'>
-            1 hour ago
+           {format(props.message?.createdAt)}
         </div>
     </div>
   )
--- client/src/pages/messenger/Messenger.jsx
+++ client/src/pages/messenger/Messenger.jsx
@@ -1,4 +1,3 @@
-
 import { useState } from "react";
 import { useEffect } from "react";
 import { useContext } from "react";
@@ -9,74 +8,116 @@ import Topbar from "../../components/topbar/Topbar";
 import { AuthContext } from "../../context/AuthContext";
 import axios from "axios";
 import "./messenger.css";
+import { useRef } from "react";
 
 export default function Messenger() {
-    const[conversations , setConversations] = useState([]);
-    const[currentChat , setCurrentChat] = useState(null);
-    const[messages , setMessages] = useState([]);
-    const {user} = useContext(AuthContext);
-    
-    useEffect(() => {
-        const getConversations = async() => {
-           try{
-            const res = await axios.get("/conversations/"+user._id);
-            setConversations(res.data);
-            // console.log(res);
-           }catch(error){
-            console.log(error);
-           }
-        }
-        getConversations()
-    },[user._id])
+  const [conversations, setConversations] = useState([]);
+  const [currentChat, setCurrentChat] = useState(null);
+  const [messages, setMessages] = useState([]);
+  const [newMessage, setNewMessage] = useState('');
+  const { user } = useContext(AuthContext);
+  const scrollRef = useRef();
 
-    return(
-        <>
-        <Topbar/>
-        <div className="messenger">
-            <div className="chatMenu">
-                <div className="chatMenuWrapper">
-                    <input placeholder="Search for friends" className="chatMenuInput" />
+  useEffect(() => {
+    const getConversations = async () => {
+      try {
+        const res = await axios.get("/conversations/" + user._id);
+        setConversations(res.data);
+        // console.log(res);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getConversations();
+  }, [user._id]);
+
+  useEffect(() => {
+    const getMessages = async () => {
+      try {
+        const res = await axios.get("/messages/" + currentChat?._id);
+        setMessages(res.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getMessages()
+  }, [currentChat]);
+
+  const handleSubmit = async(e) => {
+    e.preventDefault();
+    const message =  {
+        sender : user._id,
+        text: newMessage,
+        conversationId : currentChat._id
+    }
+    try{
+        const res = await axios.post("/messages", message);
+        setMessages([...messages, res.data]);
+        setNewMessage("")
+    }catch(error){
+        console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    scrollRef?.current?.scrollIntoView({behavior : "smooth"});
+  },[messages])
+
+
+  return (
+    <>
+      <Topbar />
+      <div className="messenger">
+        <div className="chatMenu">
+          <div className="chatMenuWrapper">
+            <input placeholder="Search for friends" className="chatMenuInput" />
+            {conversations?.map((c) => (
+              <div onClick={() => setCurrentChat(c)}>
+                <Conversations conversation={c} currentUser={user} />
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="chatBox">
+          <div className="chatBoxWrapper">
+            {currentChat ? (
+              <>
+                <div className="chatBoxTop">
                     {
-                        conversations?.map(c=>(
-                            <Conversations 
-                            conversation={c}
-                            currentUser={user}
-                            />
+                        messages.map(m => (
+                            <div ref={scrollRef}>
+                            <Message message={m} own={m.sender ===user._id} />
+                            </div>
                         ))
                     }
+                  
                 </div>
-            </div>
-            <div className="chatBox">
-                <div className="chatBoxWrapper">
-                   <div className="chatBoxTop">
-                     <Message />
-                     <Message own={true}/>
-                     <Message/>
-                     <Message/>
-                     <Message/>
-                     <Message/>
-                     <Message/>
-                     <Message/>
-                     <Message/>
-                     <Message/>
-                   </div>
-                   <div className="chatBoxBottom">
-                    <textarea className="chatMessageInput" placeholder="wwrite something">
-
-                    </textarea>
-                    <button className="chatSubmitButton">Send</button>
-                   </div>
+                <div className="chatBoxBottom">
+                  <textarea
+                    className="chatMessageInput"
+                    placeholder="wwrite something"
+                    onChange={(e) => setNewMessage(e.target.value)}
+                    value={newMessage}
+                  ></textarea>
+                  <button onClick={handleSubmit} className="chatSubmitButton">Send</button>
                 </div>
-            </div>
-            <div className="chatOnline">
-                <div className="chatWrapperOnline">
-                    <ChatOnline/>
-                    <ChatOnline/>
-                    <ChatOnline/>
-                    <ChatOnline/>
-                </div>
-            </div>
+              </>
+            ) : (
+              <span className="noConversationText">
+                Open a conversation to start chat
+              </span>
+            )}
+          </div>
         </div>
-        </>
-    )
+        <div className="chatOnline">
+          <div className="chatWrapperOnline">
+            <ChatOnline />
+            <ChatOnline />
+            <ChatOnline />
+            <ChatOnline />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
